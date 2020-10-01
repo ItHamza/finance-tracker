26b9251660c6530d3224a74eb11390cb9c1ef5bb
Ameer Hamza <ameer.hamza6555@yahoo.com>
2020-10-01 12:39:40 +0500

    Friends search functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,30 @@ class UsersController < ApplicationController
   def my_portfolio
     @stock_track = current_user.stocks
   end
+
   def my_friends
     @friends = current_user.friends
+  end
+
+  def search
+    if params[:friend].present?
+      @friends = User.search(params[:friend])
+      @friends = current_user.except_current_user(@friends)
+      if @friends
+        respond_to do |format|
+          format.js { render partial: 'users/friend_result' }
+        end
+      else
+        respond_to do |format|
+          flash.now[:alert] = "Couldn't find user"
+          format.js { render partial: 'users/friend_result' }
+        end
+      end
+    else
+      respond_to do |format|
+        flash.now[:alert] = 'Please enter a symbol to search'
+        format.js { render partial: 'users/friend_result' }
+      end
+    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -24,8 +24,36 @@ class User < ApplicationRecord
   end
 
   def full_name
-  return "#{first_name} #{last_name}" if first_name || last_name
+    return "#{first_name} #{last_name}" if first_name || last_name
 
-  'Anonymous'
+    'Anonymous'
+  end
+
+  def self.search(param)
+    param.strip!
+    to_send_back = (first_name_matches(param) + last_name_matches(param) + email_matches(param)).uniq
+    return nil unless to_send_back
+
+    to_send_back
+  end
+
+  def self.first_name_matches(param)
+    matches('first_name', param)
+  end
+
+  def self.last_name_matches(param)
+    matches('last_name', param)
+  end
+
+  def self.email_matches(param)
+    matches('email', param)
+  end
+
+  def self.matches(input_field, param)
+    where("#{input_field} like ?", "%#{param}%")
+  end
+
+  def except_current_user(users)
+    users.reject { |user| user.id == self.id }
   end
 end
